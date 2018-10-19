2df33fddacebf3baa7ec3099f47518922ac41380
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-19 16:43:35 +0800

    Create entries_controller and add routes

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,22 @@
+class EntriesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end

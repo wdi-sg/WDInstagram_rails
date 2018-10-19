6705b3fe39644bcd308b6eab4047e975406aa6a7
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-19 16:22:59 +0800

    Add Entry model and entries db migration/schema

---
+++ db/migrate/20181019081232_entries.rb
@@ -0,0 +1,9 @@
+class Entries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries do |t|
+      t.string :author
+      t.string :photo_url
+      t.string :date_taken
+    end
+  end
+end

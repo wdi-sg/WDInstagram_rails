d60d2e662ac00908844db525b3ec216a196edf57
benghui <benghui@gmail.com>
2018-10-21 00:53:29 +0800

    Completed

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,42 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    @photo.save
+    redirect_to @photo
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    @photo.update(photo_params)
+    redirect_to @photo
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+
+    redirect_to photos_path
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:author, :photo_url, :caption)
+  end
+end

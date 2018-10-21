e3d0cad9dbb0133d8bee0a8b4393bf41dc579f40
claraonmars <claraonmars@hotmail.com>
2018-10-21 18:59:31 +0800

    Added caption

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_101413) do
+ActiveRecord::Schema.define(version: 2018_10_21_103620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_10_21_101413) do
     t.string "photo_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "caption"
   end
 
 end

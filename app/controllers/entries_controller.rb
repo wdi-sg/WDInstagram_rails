5b87b608b289682b6f48546b7a904bc979ebd560
chanleyou <chanleyou@gmail.com>
2018-10-21 19:33:31 +0800

    deliberables + bonus

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,44 @@
+class EntriesController < ApplicationController
+  def index
+		@entries = Entry.all
+	end
+
+  def show
+		@entry = Entry.find(params[:id])
+	end
+
+  def new
+  end
+
+	def edit
+		@entry = Entry.find(params[:id])
+  end
+
+	def create
+		# render plain: params[:entry].inspect
+		@entry = Entry.new(entry_params)
+
+		@entry.save
+		redirect_to @entry
+  end
+
+  def update
+		@entry = Entry.find(params[:id])
+	
+		@entry.update(entry_params)
+		redirect_to @entry
+	end
+
+	def destroy
+		@entry = Entry.find(params[:id])
+		@entry.destroy
+	
+		redirect_to entries_path
+	end
+	
+	private
+  def entry_params
+    params.require(:entry).permit(:photo_url, :author, :caption, :date)
+  end
+
+end

7b16ad47b2312a04eb01d528d2decc501e1dd153
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-23 15:16:02 +0800

    fixed: error indentation and fixed picture width in show

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   end
 
   resources :comments
+
+  root 'entries#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_135648) do
+ActiveRecord::Schema.define(version: 2018_10_23_070628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "comment"
+    t.string "commentContent"
     t.bigint "entry_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

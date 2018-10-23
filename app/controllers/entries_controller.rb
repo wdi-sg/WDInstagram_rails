48af037d62a2b48712dbfbc759a23de16c5eca70
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-23 22:49:27 +0800

    Add tags checkbox to show.html

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,6 +8,8 @@ class EntriesController < ApplicationController
   def show
     @entry = Entry.find(params[:id])
     @comments = Comment.where("entry_id=?", params[:id])
+    @tags = @entry.tags
+    @alltags = Tag.all
   end
 
   def new
@@ -38,7 +40,6 @@ class EntriesController < ApplicationController
 
   def create
     @entry = Entry.new(entry_params)
-
     @entry.save
     redirect_to @entry
   end
@@ -46,7 +47,11 @@ class EntriesController < ApplicationController
   def update
     @entry = Entry.find(params[:id])
 
-    @entry.update(entry_params)
+    if params[:entry][:tag_id]
+      @entry.tags << Tag.find(params[:entry][:tag_id])
+    else
+      @entry.update(entry_params)
+    end
     redirect_to @entry
   end
 
@@ -59,6 +64,6 @@ class EntriesController < ApplicationController
 
   private
   def entry_params
-    params.require(:entry).permit(:photo_url, :author, :caption)
+    params.require(:entry).permit(:photo_url, :author, :caption, :tag_ids => [])
   end
 end

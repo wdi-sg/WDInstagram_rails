7970eeb8339e2ae76d968f5a9c5c1f9139c191e0
ChloeLiang <nliangxin@gmail.com>
2018-10-23 10:51:42 +0800

    add ffaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+(1..10).times do |i|
+  Photo.create(
+    author: FFaker::Name.name,
+    photo_url: FFaker::Image.url,
+    caption: FFaker::Lorem.paragraph,
+    date_taken: FFaker::Time.date
+  )
+
+  rand(1..10).times do
+    Comment.create(
+      text: FFaker::Lorem.sentence,
+      photo_id: i
+    )
+  end
+end

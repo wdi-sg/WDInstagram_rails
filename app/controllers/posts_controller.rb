a1c0c84166498e9ab236e61d1d0a0355af6061cc
andrealmj <andrealmj@live.com>
2019-01-25 17:23:20 +0800

    completed MVP

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,12 +18,21 @@ class PostsController < ApplicationController
     end
 
     def edit
+        @post = Post.find(params[:id])
     end
 
     def update
+        @post = Post.find(params[:id])
+
+        @post.update(post_params)
+        redirect_to @post
     end
 
     def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to root_path
     end
 
     private

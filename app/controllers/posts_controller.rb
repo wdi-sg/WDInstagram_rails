53ebe0f9fc8d1e304fe76fba78b4b4e72f13e529
ishakbhn <ishakbhn@gmail.com>
2019-01-27 16:30:49 +0800

    do get and post route for index and create

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+    render plain: params[:post].inspect
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end

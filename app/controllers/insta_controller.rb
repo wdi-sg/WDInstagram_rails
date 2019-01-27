319a703812306e453904ecb37e77e48ec6aaf264
kingcashthefifth <kingcashthefifth@gmail.com>
2019-01-27 23:07:20 +0800

    created index page

---
+++ app/controllers/insta_controller.rb
@@ -0,0 +1,30 @@
+class InstaController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+    render plain: params[:insta].inspect
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'insta#index'
+  get '/post/new' => 'insta#new', as: 'new_post'
+  post '/post' => 'insta#create', as: 'post'
+
 end

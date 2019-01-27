f7da0b065cee0d86fa3077193e6917fc030e0887
damienteo <ksteo89@gmail.com>
2019-01-27 21:01:23 +0800

    initial commit with basic crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+	root 'articles#index'
+	get '/articles/new' => 'articles#new', as: 'new_article'
+	post '/articles' => 'articles#create'
+	get '/articles/:id' => 'articles#show' , as: 'article'
+	get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
+	patch '/articles/:id' => 'articles#update'
+	delete '/articles/:id' => 'articles#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

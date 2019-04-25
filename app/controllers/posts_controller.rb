75bca35f59a5f1658f04fb2c920d4ae97898da82
chuasweechin <sampiochua@gmail.com>
2019-04-26 00:30:28 +0800

    Create the ability to make a new post using a random GIF from the Giphy API

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
+# load the gem
+require 'GiphyClient'
+require 'byebug'
+
 class PostsController < ApplicationController
   def index
-
     if request.query_parameters[:sort] == "date" && request.query_parameters[:order] == "desc"
       @posts = Post.order(created_at: :desc)
     else
@@ -14,6 +17,24 @@ class PostsController < ApplicationController
   end
 
   def new
+    api_instance = GiphyClient::DefaultApi.new
+    api_key = "IZZ1hxiEr4OSORVd7eQEfpb2SczKlS26" # String | Giphy API Key.
+
+    opts = {
+      tag: "Attack on Titan", # String | Filters results by specified tag.
+      rating: "g", # String | Filters results by specified rating.
+      fmt: "json" # String | Used to indicate the expected response format. Default is Json.
+    }
+
+    begin
+      #Random Endpoint
+      # @result = api_instance.gifs_random_get(api_key, opts)
+      @result = "https://media2.giphy.com/media/uiMTiXW94z280/giphy.gif"
+
+    rescue GiphyClient::ApiError => e
+      puts "Exception when calling DefaultApi->gifs_random_get: #{e}"
+    end
+
   end
 
   def create
@@ -43,7 +64,7 @@ class PostsController < ApplicationController
 
 private
   def post_params
-    params.require(:post).permit(:author_name, :photo_url, :title, :caption, :created_at)
+    params.require(:post).permit(:author_name, :photo_url, :title, :caption, :gif_url)
   end
 
 end

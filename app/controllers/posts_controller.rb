3f402a9dd93bdc9a9753bac1eb2aefa4ed696167
khyreerusydi <nicoraventbh@gmail.com>
2019-04-27 17:29:00 +0800

    created delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,13 @@ class PostsController < ApplicationController
     redirect_to @post
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :author_name, :photo_url, :caption)

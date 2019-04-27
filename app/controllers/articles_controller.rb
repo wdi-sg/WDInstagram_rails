691cdc5e0614f28b3ab2edba12fa254ff656ed93
shiowwj <shiowweijun@gmail.com>
2019-04-27 23:47:11 +0800

    Added random gif post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,6 @@
+require 'GiphyClient'
+require 'json'
+
 class ArticlesController < ApplicationController
 
   def index
@@ -7,7 +10,7 @@ class ArticlesController < ApplicationController
         @results.push(post)
       end
 
-    if params[:order] === nil || params[:order] =="des"
+    if params[:order] === nil || params[:sort] == "date" && params[:order] =="des"
       puts "hello #{params}"
       puts @results
       @results = @results.sort_by {|result| result[:created_at]}.reverse
@@ -23,6 +26,30 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    begin
+      api_instance = GiphyClient::DefaultApi.new
+      api_key = "UTAiBBivgvVDoEVhGDIN3FKIRl7AacmU" # String | Giphy API Key.
+
+      opts = {
+        # tag: "burrito", # String | Filters results by specified tag.
+        # rating: "g", # String | Filters results by specified rating.
+        limit: 25,
+        # fmt: "json" # String | Used to indicate the expected response format. Default is Json.
+      }
+
+      # start API call
+      begin
+    #Categories Endpoint.
+        @result = api_instance.gifs_categories_get(api_key, opts)
+
+        # random GIF
+        randomNumber = rand(1..25)
+        @test = @result.data[randomNumber].gif.images.fixed_height.url
+      # end of api call
+      rescue GiphyClient::ApiError => e
+        puts "Exception when calling DefaultApi->gifs_categories_get: #{e}"
+      end
+    end
   end
 
   def edit

0bdb27de9325344606d489fd97babf206e28efbb
seeo <aragonlotr5@gmail.com>
2019-04-28 15:34:27 +0800

    did up the migrate file, this time with caption column name

---
+++ db/migrate/20190428072349_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :photo_url
+      t.text :author_name
+      t.text :caption
+      t.timestamps
+    end
+  end
+end

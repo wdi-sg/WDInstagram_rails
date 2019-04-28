bff6bf31178200b8aa91bbff7225a8f922efb90d
seeo <aragonlotr5@gmail.com>
2019-04-28 22:10:54 +0800

    included an images folder in the public folder. has an up and down arrow png each

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
         #adding a css class to create small arrows for sorting table
         css_class = column == sort_column ? "current  #{sort_direction}" :nil
         direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-        link_to title, :sort => column, :direction => direction
+        link_to title, {:sort => column, :direction => direction}, {:class => css_class}
     end
 end

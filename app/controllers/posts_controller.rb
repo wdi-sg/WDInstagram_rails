3030382e3a1b8ed310aff2adc811d66ca14ce989
seeo <aragonlotr5@gmail.com>
2019-04-29 00:25:45 +0800

    trying to get my api_key (giphy) i secrets.yml file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,6 @@
+require 'GiphyClient'
+require 'byebug'
+
 class PostsController < ApplicationController
     helper_method :sort_column, :sort_direction
     
@@ -10,7 +13,26 @@ class PostsController < ApplicationController
     end
 
     def new
+        api_instance = GiphyClient::DefaultApi.new
 
+        api_key = ENV['GIPHYCLIENT_API_KEY']# String | Giphy API Key.
+
+        q = "cheeseburgers" # String | Search query term or prhase.
+
+        opts = { 
+        limit: 25, # Integer | The maximum number of records to return.
+        offset: 0, # Integer | An optional results offset. Defaults to 0.
+        rating: "g", # String | Filters results by specified rating.
+        lang: "en", # String | Specify default country for regional content; use a 2-letter ISO 639-1 country code. See list of supported languages <a href = \"../language-support\">here</a>.
+        fmt: "json" # String | Used to indicate the expected response format. Default is Json.
+        }
+        begin
+            #Search Endpoint
+            result = api_instance.gifs_search_get(api_key, q, opts)
+            p result
+            rescue GiphyClient::ApiError => e
+            puts "Exception when calling DefaultApi->gifs_search_get: #{e}"
+        end
     end
 
     def create

5b0964e1b0e3db3b418c29b05df2007e295f8c71
seantanjs <seantanjuisiang@gmail.com>
2019-04-28 23:39:37 +0800

    completed deliverables only

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+        root 'posts#index'
+
+        # CREATE (C)
+        get '/posts/new' => 'posts#new', as: 'new_post'
+        post '/posts' => 'posts#create'
+
+        # READ (R)
+        get '/posts/:id' => 'posts#show' , as: 'post'
+
+        # UPDATE (U)
+        get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+        patch '/posts/:id' => 'posts#update'
+
+        # DELETE (D)
+        delete '/posts/:id' => 'posts#destroy'
+
+      end

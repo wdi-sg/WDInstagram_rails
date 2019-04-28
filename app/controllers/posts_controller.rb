095c92f1b63460de62b85d93bf8daa6edeaadbee
seeo <aragonlotr5@gmail.com>
2019-04-28 12:40:09 +0800

    created the seven restful routes BUT empty

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,30 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+    
+    end
+    
 end

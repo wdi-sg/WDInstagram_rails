765dbbf04c9efbab6839beca7e531d11e8101544
claucanchin <claucanchin@gmail.com>
2019-04-28 13:21:23 +0800

    done with basic crud

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,23 @@ Rails.application.routes.draw do
 
   # index (displays all entries)
   root 'posts#index'
-  # show (displays a specific post)
-  # get '/posts/:id' => 'posts#show' , as: 'post'
+
   # new (displays a form to create a new post)
   get '/posts/new' => 'posts#new', as: 'new_post'
+
   # create (saves a new post it to the database)
   post '/posts' => 'posts#create'
+
+  # show (displays a specific post)
+  get '/posts/:id' => 'posts#show' , as: 'post'
+
   # edit (displays a form for editing a particular post)
-  # get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+
   # update (takes input from the edit form and updates the post in the db appropriately)
-  # patch '/posts/:id' => 'posts#update'
+  patch '/posts/:id' => 'posts#update'
+
   # destroy (deletes a specific post from the database)
-  # delete '/posts/:id' => 'posts#destroy'
+  delete '/posts/:id' => 'posts#destroy'
 
 end

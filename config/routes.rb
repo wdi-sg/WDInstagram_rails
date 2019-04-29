da6c471b629a0f525d318c9e4e584146e80b7951
LL <limwenyaolester@hotmail.com>
2019-04-29 08:56:59 +0800

    mvp

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
     get '/articles/new' => 'articles#new', as: 'new_article'
 
     post '/articles' => 'articles#create'
+
+    get '/articles/:id' => 'articles#show' , as: 'article'
+
+    get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
+
+    patch '/articles/:id' => 'articles#update'
+
+    delete '/articles/:id' => 'articles#destroy'
 end
--- db/migrate/20190425235256_articles.rb
+++ db/migrate/20190425235256_articles.rb
@@ -1,4 +1,10 @@
 class Articles < ActiveRecord::Migration[5.2]
   def change
+    create_table :articles do |t|
+      t.string :author_name
+      t.text :photo_url
+      t.text :title
+      t.timestamps
+    end
   end
 end

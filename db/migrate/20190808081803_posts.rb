90c4ba801dda3b718791588bf041e0bf55783dcb
Yi Xin Ang <angyixin@gmail.com>
2019-08-08 21:03:23 +0800

    first cut crud app done

---
+++ db/migrate/20190808081803_posts.rb
@@ -0,0 +1,11 @@
+class Posts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :author_name
+      t.string :photo_url
+      t.text :text
+      t.timestamps
+    end
+  end
+end

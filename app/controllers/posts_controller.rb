0b0923e681e17b81f9fb2f6b689bfcb1822e47d4
“eenfeeneet” <burnhafiz@gmail.com>
2019-08-10 05:07:51 +0800

    diff added delete button.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   end
 
   def edit
-    #@article=Article.find(params[:id])
+    @post=Post.find(params[:id])
   end
 
   def create
@@ -26,15 +26,18 @@ class PostsController < ApplicationController
   end
 
   def update
-    #@article = Article.find(params[:id])
+    @post = Post.find(params[:id])
 
-    #@article.update(article_params)
-    #redirect_to @article
+    @post.update(post_params)
+    redirect_to @post
   end
 
 
   def destroy
+    @post = Post.find(params[:id])
 
+    @post.destroy()
+    redirect_to posts_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'posts#index', as: 'home'
-  #get '/posts' => 'posts#index', as: 'posts'
+  get '/posts' => 'posts#index', as: 'posts'
   #post '/posts' => 'posts#create'
   get '/posts/new' => 'posts#new', as: 'new_post'
+
 
 
   resources :posts

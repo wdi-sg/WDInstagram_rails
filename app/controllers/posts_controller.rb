4eb8dd63014cdf6f867d0a5e24fb3fba381f8082
“eenfeeneet” <burnhafiz@gmail.com>
2019-08-10 04:14:55 +0800

    diff added new, show views. added new, show routes.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
 
   def index
-    #@posts = Article.all
+    @posts = Post.all
   end
 
   def show
-     #@article = Article.find(params[:id])
+     @post = Post.find(params[:id])
   end
 
   def new
@@ -17,12 +17,11 @@ class PostsController < ApplicationController
   end
 
   def create
-    #render plain: params[:article].inspect
+    #render plain: params[:post].inspect
+    @post = Post.new(post_params)
 
-    #@article = Article.new(article_params)
-
-    #@article.save
-    #redirect_to @article
+    @post.save
+    redirect_to @post
 
   end
 
@@ -36,6 +35,11 @@ class PostsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :author_name, :img_url)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'posts#index'
+  root 'posts#index', as: 'home'
+  #get '/posts' => 'posts#index', as: 'posts'
+  #post '/posts' => 'posts#create'
+  get '/posts/new' => 'posts#new', as: 'new_post'
+
 
   resources :posts
 end

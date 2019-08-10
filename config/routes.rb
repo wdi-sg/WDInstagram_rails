f786b0bf76304cdd02975061bdfc573e4757e6c4
thea-fan <julythefourteen@gmail.com>
2019-08-10 19:43:29 +0800

    base app completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
+  get '/posts/new' => 'posts#new', as: "new_post"
+  post '/posts' => 'posts#create'
+  get '/posts/:id' => 'posts#show' , as: 'post'
+  get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  patch '/posts/:id' => 'posts#update'
+  delete '/posts/:id' => 'posts#destroy'
+
+
 end

45067cc7a61dacca70289278ca92f4151f655315
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-08-10 21:43:02 +0800

    made photos route and index and new

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,27 @@
+class PhotosController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'photos#index'
+  get '/photos/new' => 'photos#new', as: 'new_photo'
 end

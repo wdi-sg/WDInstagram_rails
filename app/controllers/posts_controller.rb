7b53522f66f73f7850928c345e49498409d1e84f
MARCYKAY <mahchinkok@gmail.com>
2019-08-11 16:06:47 +0800

    commit: sorting done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
     def index
         @posts = Post.all
+        puts "######################################"
+        puts @conds
+        puts @order
+
     end
 
     def show
         @post = Post.find(params[:id])
+    end
+
+    def sort
+        @posts = Post.all
+        @conds = params[:conds]
+        @order = params[:order]
+        puts @conds
+        puts @order
+        if @order == "asc"
+            @posts = @posts.sort_by {|a| a[@conds]}
+        else
+            @posts = @posts.sort_by {|a| a[@conds]}.reverse
+        end
+        render :index
     end
 
     def new
@@ -39,4 +57,6 @@ class PostsController < ApplicationController
         def post_params
             params.require(:post).permit(:author_name, :photo_url, :title, :caption)
         end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'posts#index'
+    get 'posts/sort/:conds/:order' => 'posts#sort', as: 'sort_post'
     get '/posts/new' => 'posts#new', as: 'new_post'
     post '/posts' => 'posts#create'
     get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
     get '/posts/:id' => 'posts#show' , as: 'post'
     patch '/posts/:id' => 'posts#update'
     delete '/posts/:id' => 'posts#destroy'
+
 end

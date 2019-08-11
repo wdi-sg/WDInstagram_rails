ecb36031e29df5a4d7a22295f59c928bdcc9793d
MalcolmLow83 <malcomlow@gmail.com>
2019-08-11 18:44:04 +0800

    Completed basic deliverables

---
+++ db/migrate/20190811064555_posts.rb
@@ -0,0 +1,11 @@
+class Posts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+    t.string :title
+    t.text :author_name
+    t.text :photo_url
+    t.text :comment
+    t.timestamps
+    end
+  end
+end

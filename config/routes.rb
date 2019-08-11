fb6e86eeb51ba09114f4e4be2220d1bc4faf93c9
Jonathan <jonathanbek93@gmail.com>
2019-08-11 21:36:04 +0800

    did basic, caption, timestamp. did non-variable order on index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+
+  #create (create info from new into db)
+  post '/posts' => 'posts#create'
+  #create form
+  get '/posts/new' => 'posts#new', as: 'new_post'
+  #show single
+  get '/posts/:id' => 'posts#show', as: 'post'
+  #update (edited info into db), no html.erb
+  patch '/posts/:id' => 'posts#update'
+  #edit
+  get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  #delete, no html.erb
+  delete '/posts/:id' => 'posts#destroy'
+
+  get '/posts/:sort' => 'posts#sortIndex', as: 'sort_post'
+
+
 end

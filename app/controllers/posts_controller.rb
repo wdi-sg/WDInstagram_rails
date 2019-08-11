a599897e8afc42af004e7c54a1011a450c8b5441
thea-fan <julythefourteen@gmail.com>
2019-08-11 16:53:25 +0800

    sort by date function added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,24 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+
+    # @posts = Post.all
+    # @posts = Post.order(updated_at: :asc)
+
+
+    @posts = [];
+
+    Post.all.each do |post|
+      @posts.push(post)
+    end
+
+    if request.query_parameters[:sort] == "date" && request.query_parameters[:order] == "asc"
+      @posts = @posts.sort_by { |result| result[:updated_at] }
+    elsif request.query_parameters[:sort] == "date" && request.query_parameters[:order] == "desc"
+      @posts = @posts.sort_by { |result| result[:updated_at] }.reverse
+    end
+
+
+
   end
 
   def show

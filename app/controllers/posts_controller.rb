499b48d13c8214f7438cf8545ef7953e5816fd10
“eenfeeneet” <burnhafiz@gmail.com>
2019-08-12 00:43:43 +0800

    diff added sort functionality.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order_list(params[:sort])
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+
+  def self.order_list(sort_order)
+    if sort_order == "latest" || sort_order.blank?
+      order(created_at: :desc)
+    elsif sort_order == "title"
+      order(title: :asc)
+    else
+      order(created_at: :asc)
+    end
+  end
 end

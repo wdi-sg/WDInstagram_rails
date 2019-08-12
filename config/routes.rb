afcf914264ca4756c3e2698bfe1c900783d2fd76
Nicholas Bingei <nbinged@gmail.com>
2019-08-12 13:07:20 +0800

    Commit 1: Routes, SHOW, NEW and CREATE work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # You can have the root of your site routed with "root"
+root 'posts#index'
+
+  # Example of regular route:
+get 'show' => 'pages#show'
+
+  # Example of 'resource'
+resources :posts
+
+
 end

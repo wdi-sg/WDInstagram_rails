f870df6531915b552780a7109687cb712b311bd2
Nicholas Bingei <nbinged@gmail.com>
2019-08-12 16:57:43 +0800

    Commit 2: Base complete. Some bootstrap added too

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # You can have the root of your site routed with "root"
-root 'posts#index'
+root 'posts#index', as: 'home'
 
   # Example of regular route:
-get 'show' => 'pages#show'
+get 'show' => 'pages#show', as: 'show'
 
   # Example of 'resource'
 resources :posts

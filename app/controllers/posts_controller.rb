585674ddedc9546987304ac560584d8ee8274589
EliseChia <swedesurrender@gmail.com>
2019-08-12 23:01:03 +0800

    done with show, edit, delete. completed basic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,34 @@ class PostsController < ApplicationController
   def new
   end
 
+  def edit
+    @post =Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
     @post.save
-    render plain: params[:post].inspect
-    #redirect_to root_path
+    # render plain: params[:post].inspect
+    redirect_to root_path
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
   def post_params
     params.require(:post).permit(:title, :author_name, :photo_url)
   end

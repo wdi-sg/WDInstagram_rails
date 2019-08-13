53bba538c7da1eee45dbe530fc17f3287e44318a
aikchongtan <aikchongtan@yahoo.com>
2019-08-13 11:04:45 +0800

    Initial commit.

---
+++ app/models/video.rb
@@ -0,0 +1,3 @@
+class Video < ActiveRecord::Base
+    # AR classes are singular and capitalized by convention
+  end
---
+++ db/migrate/20190812133041_drop_posts.rb
@@ -0,0 +1,6 @@
+class DropPosts < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :posts
+
+  end
+end

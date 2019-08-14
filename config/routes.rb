7652f3703b7bf878393ad5f06c95367daecae53f
caspianzx <caspianzx@gmail.com>
2019-08-14 16:17:43 +0800

    created WDInstagram restful app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+  get '/posts/new' => 'posts#new', as: 'new_posts'
+  post '/posts' => 'posts#create', as:'posts'
+  get '/posts/:id' => 'posts#show' , as: 'post'
+  get '/posts/:id/edit' => 'posts#edit', as: 'edit_posts'
+  patch '/posts/:id' => 'posts#update'
+  delete '/posts/:id' => 'posts#destroy'
+  # resources :posts
 end

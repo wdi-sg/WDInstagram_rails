eb8b92f84bc9e5bd5386168bd22bc11142654997
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-01 17:09:42 +0800

    Added route to get the form for a new post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
+
+  get 'posts/new' => 'posts#new', as: 'new_post'
 end

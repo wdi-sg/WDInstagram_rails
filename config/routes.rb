21763d7f56e40e1313e0cdcf80eaaa97701edf37
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-01 22:50:18 +0800

    Added route to get edit form

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/posts/new' => 'posts#new', as: 'new_post'
   post '/posts' => 'posts#create'
   get '/posts/:id' => 'posts#show', as: 'post'
+  get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
 end

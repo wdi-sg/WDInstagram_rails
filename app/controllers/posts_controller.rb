23e154188ec90166dccd90caf2b94ea1e3f5ac48
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-01 23:17:08 +0800

    Updated destroy method to delete post from db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,6 +35,10 @@ class PostsController < ApplicationController
     end
 
     def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to root_path
     end
 end
 

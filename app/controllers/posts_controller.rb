39a4ac057cbb04ffac1afa37d5a0366e0d9e07d1
lhjd <hongjinlau@gmail.com>
2019-11-01 16:53:57 +0800

    Create root route

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+    def index
+        render plain: "hello"
+    end
+  
+    def show
+    end
+  
+    def new
+    end
+  
+    def edit
+    end
+  
+    def create
+    end
+  
+    def update
+    end
+  
+    def destroy
+    end
+  end
+  

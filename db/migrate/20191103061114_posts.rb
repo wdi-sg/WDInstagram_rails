b30b619fadb07f4a724924c049c34cf575bf78c7
kevinngth <kevin.ng.th@gmail.com>
2019-11-03 16:30:36 +0800

    basic deliverables done

---
+++ db/migrate/20191103061114_posts.rb
@@ -0,0 +1,11 @@
+class Posts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :photo_url
+      t.text :author_name
+      t.text :caption
+      t.timestamps
+    end
+  end
+end

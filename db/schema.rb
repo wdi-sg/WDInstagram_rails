c8130dd22028ae377161a4718422b98f8406bea8
garricktgh <garrick@hotmail.sg>
2019-11-03 16:51:17 +0800

    complete new comment feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_054411) do
+ActiveRecord::Schema.define(version: 2019_11_03_063935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "username"
+    t.text "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "author_name"
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2019_11_03_054411) do
     t.text "caption"
   end
 
+  add_foreign_key "comments", "posts"
 end

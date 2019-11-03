5c36661d10b6bee51b9a24c306dc7daac8ea6bb5
jasminesis <jasmine-aips@hotmail.com>
2019-11-03 19:16:58 +0800

    couldn't add gif api so added random gif

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,14 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require 'net/http'
+require 'json'
+url="http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=3TM9W92RMV3BlV2c8ssMAh5swl9qqS1E&limit=5"
+resp = Net::HTTP.get_response(URI.parse(url))
+buffer = resp.body
+result = JSON.parse(buffer)
+puts result
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)

837b5339a7feda14af1f47ad7374b4f4635f1a08
joyce-paul <joyce_paul@hotmail.com>
2020-02-10 17:22:26 +0800

    Completed MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+    get '/posts/new' => 'posts#new', as: 'new_post'
+
+    post '/posts' => 'posts#create'
+
+    get '/posts/:id' => 'posts#show' , as: 'post'
+
+    get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+
+    patch '/posts/:id' => 'posts#update'
+
+    delete '/posts/:id' => 'posts#destroy'
+
+    root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

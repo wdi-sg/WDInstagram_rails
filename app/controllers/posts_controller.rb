69a3d5651832fc6bd70b6895aeb9abbaad506a4f
rachellesg <rachelle.yeo@outlook.com>
2020-02-10 22:04:54 +0800

    trying to use GIPHY api

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,35 @@
 require "net/http"
 require "json"
 
+# load the gem
+require "GiphyClient"
+
+api_instance = GiphyClient::DefaultApi.new
+
+api_key = "dc6zaTOxFJmzC" # String | Giphy API Key.
+
+q = "wwe" # String | Search query term or prhase.
+
+opts = {
+  limit: 1, # Integer | The maximum number of records to return.
+  offset: 0, # Integer | An optional results offset. Defaults to 0.
+  rating: "g", # String | Filters results by specified rating.
+  lang: "en", # String | Specify default country for regional content; use a 2-letter ISO 639-1 country code. See list of supported languages <a href = \"../language-support\">here</a>.
+  fmt: "json", # String | Used to indicate the expected response format. Default is Json.
+}
+
+begin
+  #Search Endpoint
+  result = api_instance.gifs_search_get(api_key, q, opts)
+  #   puts result.data[0].images.fixed_width_downsampled.url
+  random_gif = result.data[0].images.fixed_width_downsampled.url
+rescue GiphyClient::ApiError => e
+  puts "Exception when calling DefaultApi->gifs_search_get: #{e}"
+end
+
+puts "HeLLLO"
+puts random_gif
+
 class PostsController < ApplicationController
   def index
     puts params
@@ -17,16 +46,13 @@ class PostsController < ApplicationController
   end
 
   def show
+    puts params
+    puts @random_gif
     @post = Post.find(params[:id])
   end
 
   def new
-    url = "http://api.giphy.com/v1/gifs/search?q=wwe&api_key=dc6zaTOxFJmzC&limit=1"
-    resp = Net::HTTP.get_response(URI.parse(url))
-    buffer = resp.body
-    result = JSON.parse(buffer)
     puts "Hello"
-    puts result[1]
   end
 
   def edit

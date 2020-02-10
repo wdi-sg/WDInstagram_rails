f6bce136d14de72a6faa6d8503531dc7e2575ed2
SamTanFL <samsite_tan@hotmail.com>
2020-02-10 23:25:45 +0800

    copy pasted and adapted

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    root 'posts#index'
+
+    get '/posts/new' => 'posts#new', as: 'new_posts'
+
+    get '/posts/:id/edit' => 'posts#edit', as: 'edit_posts'
+
+    get '/posts/:id' => 'posts#show' , as: 'posts'
+
+    post '/posts' => 'posts#create'
+
+    patch '/posts/:id' => 'posts#update'
+
+    delete '/posts/:id' => 'posts#destroy'
+
 end

2a0648aab3a473adea5fd5f17995b7278b71d329
Robert Kolsek <kolsek.income@gmail.com>
2020-02-10 23:55:27 +0800

    - Added random giphy GIF via API

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+
+
+
   def index
-    
-    # add html a tag to index.erb with link and parameters to sort by
-    # take these paremeters and run if statements
-    # if sort_by is a certain way, run this Post.all.order, else another one
     sort_by = params[:sort]
+
+    puts @result
 
     if sort_by == "date"
       @posts = Post.all.order("created_at DESC")

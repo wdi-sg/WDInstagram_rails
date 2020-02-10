08ca1a864ade8397c1c9ca3fd7a1feed5dc32cfe
rachellesg <rachelle.yeo@outlook.com>
2020-02-10 16:40:07 +0800

    able to update posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -25,6 +26,10 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    @post.update(post_params)
+    redirect_to @post
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "/posts/new" => "posts#new", as: "new_post"
   post "/posts" => "posts#create"
   get "/posts/:id" => "posts#show", as: "post"
+  get "/posts/:id/edit" => "posts#edit", as: "edit_post"
+  patch "/posts/:id" => "posts#update"
 
   root "posts#index"
 end

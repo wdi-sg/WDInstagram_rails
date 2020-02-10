9d3048c8d2add155ee6b5f92c982cd75e063434b
rachellesg <rachelle.yeo@outlook.com>
2020-02-10 16:33:52 +0800

    shows individual posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/posts/new" => "posts#new", as: "new_post"
   post "/posts" => "posts#create"
-  # get "/posts/:id" => "posts#show", as: "posts"
+  get "/posts/:id" => "posts#show", as: "post"
 
   root "posts#index"
 end

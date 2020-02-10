ddea663a63193e112b90dcc18b5eca5ed6284290
Robert Kolsek <kolsek.income@gmail.com>
2020-02-10 17:31:46 +0800

    - Added timestamps to table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_085535) do
+ActiveRecord::Schema.define(version: 2020_02_10_092756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_02_10_085535) do
     t.text "photo_url"
     t.string "title"
     t.string "caption"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

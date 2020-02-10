d843e237c8bef3017d93efd32e7a3cee53baa7c4
Angel Ferreros <AngelFerreros@tjs-macbook-pro.lan>
2020-02-10 21:54:57 +0800

    Create,Read,Update done

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def new
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def create
+      @post = Post.new(post_params)
+
+      @post.save
+      redirect_to @post
+    end
+
+    def update
+      @post = Post.find(params[:id])
+
+      @post.update(post_params)
+      redirect_to @post
+    end
+
+    def destroy
+    end
+
+    private
+      def post_params
+        params.require(:post).permit(:author, :photo, :title)
+      end
+
+end

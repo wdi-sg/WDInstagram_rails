d46fcc83f9da09db7690d2bb9a9caef041fcaf84
wickker <choydianchun@gmail.com>
2020-05-08 18:55:15 +0800

    able to display new post page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
+
+  get '/posts/new' => 'posts#new'
+
+  post '/posts' => 'posts#create'
+
+  get '/posts/:id' => 'posts#show'
+
+  get '/posts/:id/edit' => 'posts#edit'
+
+  patch '/posts/:id' => 'posts#update'
+
+  delete '/posts/:id' => 'posts#destroy'
+
 end

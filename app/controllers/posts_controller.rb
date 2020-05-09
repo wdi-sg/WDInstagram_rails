8ea621f3f0593c974ec5221551b7a8cc36f681aa
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-05-09 19:13:08 +0800

    1. updated gemfiles and bundler. 2. initialized DB 3. Able to create and show

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :photo_url, :caption)
+    end
+end

9c4a5c316bcf846a17af5cc0faa401baf6b7922f
kokchuantan <kokchuan1996@gmail.com>
2020-05-10 20:37:43 +0800

    added show all posts function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,30 @@
 class PostsController < ApplicationController 
+    def index      
+        @posts = Post.all
+    end
+
+    def show
+
+    end
     
+    def new
+
+    end
+    
+    def edit
+
+    end
+    
+    def create
+
+    end
+    
+    def destroy
+
+    end
+    
+    private
+    def post_params
+    params.require(:post).permit(:author,:photo_url,:text)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  get '/posts/new' => 'layouts#new', as: 'new_post'
-  post '/posts' => 'layouts#create'
-  get '/posts/:id' => 'layouts#show' , as: 'post'
-  get '/posts/:id/edit' => 'layouts#edit', as: 'edit_post'
-  patch '/posts/:id' => 'layouts#update'
-  delete '/posts/:id' => 'layouts#destroy'
+  get '/posts/new' => 'posts#new', as: 'new_post'
+  post '/posts' => 'posts#create'
+  get '/posts/:id' => 'posts#show' , as: 'post'
+  get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  patch '/posts/:id' => 'posts#update'
+  delete '/posts/:id' => 'posts#destroy'
 end

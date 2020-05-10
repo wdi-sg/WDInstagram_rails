104b01a964903dcd36f4dafa51dbecd5b723a0f0
qundax <qunda.lim@outlook.sg>
2020-05-11 00:17:55 +0800

    sei-22-qunda

---
+++ db/migrate/20200510152535_posts.rb
@@ -0,0 +1,11 @@
+class Posts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :author_name
+      t.text :photo_url
+      t.string :title
+      t.text :caption
+      t.timestamps
+    end
+  end
+end

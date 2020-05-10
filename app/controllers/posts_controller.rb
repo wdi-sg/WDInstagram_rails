6298ab1897e0a81d2c605726c76febe5aa9f7fdf
mdazharaw <darkaz62@hotmail.com>
2020-05-10 23:15:17 +0800

    Initial Commit
    Repeated article exercise

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+      end
+  
+    def show
+        @post = Post.find(params[:id])
+      end
+  
+    def new
+    end
+  
+    def edit
+        @post = Post.find(params[:id])
+      end
+  
+    def create
+        @post = Post.new(post_params)
+      
+        @post.save
+        redirect_to @post
+      end
+  
+    def update
+    end
+  
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+    
+      redirect_to root_path
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :text)
+     end
+
+  end
---
+++ db/migrate/20200510143759_posts.rb
@@ -0,0 +1,9 @@
+class Posts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.timestamps
+    end
+  end
+end

b7c389d46c0d1e78d134ddec5d34267b44632376
marctanyh <marctanyh@gmail.com>
2020-05-11 09:21:31 +0800

    Marcus - WDInstagram_rails
    MVP

---
+++ db/migrate/20200510145350_articles.rb
@@ -0,0 +1,10 @@
+class Articles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :author
+      t.text :photo_url
+      t.text :title
+      t.timestamps
+    end
+  end
+end
